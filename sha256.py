5b961d1a27f5a75c0fc087d6bba9e9f8a7a60a0d
ZacharyLiu-CS <liuzhenm@mail.ustc.edu.cn>
2020-05-15 13:08:54 +0800

    Laila add the blockchain code

--- sha256.py
+++ sha256.py
@@ -252,9 +252,14 @@ def test():
     a_str = "just a test string"
     
     assert 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' == sha256().hexdigest()
+    print("source str: %s\nsha256 str: %s\n"%('None',sha256().hexdigest()))
+
     assert 'd7b553c6f09ac85d142415f857c5310f3bbbe7cdd787cce4b985acedd585266f' == sha256(a_str).hexdigest()
-    assert '8113ebf33c97daa9998762aacafe750c7cefc2b2f173c90c59663a57fe626f21' == sha256(a_str*7).hexdigest()
     
+    print("source str: %s\nsha256 str: %s\n"%(a_str,sha256(a_str).hexdigest()))
+    assert '8113ebf33c97daa9998762aacafe750c7cefc2b2f173c90c59663a57fe626f21' == sha256(a_str*7).hexdigest()
+    print("source str: %s\nsha256 str: %s\n"%(a_str*7,sha256(a_str*7).hexdigest()))
+ 
     s = sha256(a_str)
     s.update(a_str)
     assert '03d9963e05a094593190b6fc794cb1a3e1ac7d7883f0b5855268afeccc70d461' == s.hexdigest()
